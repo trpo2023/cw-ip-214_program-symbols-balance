459504d02e5aee7e3ed85fb021909065fd86b89f
Kojjia <nik.nikita2132@mail.ru>
2023-09-18 11:41:29 +0700

    Create tests, #5

---
+++ tests/test_balance_checker.py
@@ -0,0 +1,35 @@
+from src.modules.balance_checker import BalanceChecker
+
+import pytest
+
+@pytest.fixture
+def balance_checker():
+    return BalanceChecker()
+
+def test_balanced_brackets(balance_checker):
+    input_str = "{[()]}"
+    result = balance_checker.check_partial(input_str)
+    assert not result.error
+
+def test_unmatched_closing_bracket(balance_checker):
+    input_str = "{[()]}}"
+    result = balance_checker.check_partial(input_str)
+    assert result.error == "Unmatched closing bracket"
+    assert result.bracket_index == 6
+
+def test_mismatched_closing_bracket(balance_checker):
+    input_str = "{[(])}"
+    result = balance_checker.check_partial(input_str)
+    assert result.error == "Mismatched closing bracket"
+    assert result.bracket_index == 3
+
+def test_unmatched_opening_bracket(balance_checker):
+    input_str = "{[()]"
+    result = balance_checker.check_partial(input_str)
+    assert result.error == "Unmatched opening bracket"
+    assert result.bracket_index == 0
+
+def test_no_errors(balance_checker):
+    input_str = "#include \"stdio.h\"\n\r int a = 5;\t\t\tint b = 3;"
+    result = balance_checker.check_partial(input_str)
+    assert not result.error
--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual(123, 123)
-
-if __name__ == '__main__':
-    unittest.main()
